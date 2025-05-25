9ad4f1e43d519b889109658899f35f132919f949
RFlx <temospena@gmail.com>
2025-05-25 19:14:38 +0100

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# EITbraga
- Digitalizing Mobility - GTFS and other open data for mobility planning
+# PGmob360
+
+Materiais e dados para as aulas de **Literacia sobre dados abertos em transportes**, módulo da [Pós-Graduação Mobilidade 360º Gestão de Transportes](https://execed.iscte-iul.pt/applied-online-pos-graduacao-em-mobilidade-360-gestao-de-transportes) do Iscte Executive Education.
+
+![](images/clipboard-209231891.png)
+
+## Literacia sobre dados abertos em transportes
+
+1.  Introdução ao R
+2.  Importação e limpeza de dados de transportes em R
+3.  Mapas interativos em R
+4.  Os dados abertos em mobilidade e transportes
+5.  Exemplos nacionais e internacionais de análises com dados abertos
+6.  Introdução ao GTFS
+7.  Fontes de dados GTFS e tratamento
+8.  Acessibilidade espacial em R
+9.  Boas práticas de partilha e abertura de dados de mobilidade e transportes
+10. Exercício prático utilizando dados de operadora de transportes
+
+Ver **website** em [rosamfelix.github.io/PGmob360](https://rosamfelix.github.io/PGmob360/)
+
+### Citar como
+
+```         
+Félix, R. (2024). Literacia sobre dados abertos em transportes - módulo da Pós-Graduação Mobilidade 360º Gestão de Transportes do ISCTE Executive Education. https://rosamfelix.github.io/PGmob360/
+```
---
+++ code/dataprep_gtfs.R
@@ -0,0 +1,191 @@
+# objetivo: ler dados de um arquivo GTFS e visualizar as paragens e rotas
+
+# Pacotes
+# install.packages("tidytransit")
+library(tidyverse)
+library(tidytransit)
+library(mapview)
+
+# Ler dados
+METRO = read_gtfs("original/metro_gtfs.zip")
+validate_gtfs(METRO) # validar
+
+# Ver os dados
+
+# Paragens
+View(METRO$stops)
+
+ESTACOES_metro = stops_as_sf(METRO$stops) # converter texto para geometria
+# ESTACOES_metro = ESTACOES_metro |> filter(parent_station == "") # remover aquelas subsidiarias
+mapview(ESTACOES_metro) # visualizar
+
+## Desafio ##
+# Ver que estações de metro se consegue alcançar em 15, 30 e 45 minutos a partir da Baixa
+# 1. num dia útil em hora de ponta
+# 2. num domingo de tarde
+
+
+# 1. num dia útil em hora de ponta
+
+# filtramos os dados para o dia de hoje entre as 7h30 e as 9h00
+stop_times_metro = filter_stop_times(METRO, "2024-11-20", "07:30:00", "09:00:00")
+duracao_viagem = travel_times(stop_times_metro, "Baixa / Chiado") # Baixa / Chiado é a estação de metro da Baixa
+
+summary(duracao_viagem$travel_time/60) # resumo em minutos
+
+duracao_viagem_BC_HP = ESTACOES_metro |> 
+  left_join(duracao_viagem, by = c("stop_id" = "to_stop_id")) |>  # juntar os dados 
+  filter(!is.na(travel_time)) # remover as plataformas não usadas
+
+# mapa com as estações que se conseguem alcançar em 15, 30 e 45 minutos
+ggplot(duracao_viagem_BC_HP) +
+  geom_sf(aes(color = travel_time/60))+
+  ggtitle("Alcance desde a estação Baixa-Chiado",
+          subtitle = "às 7h30 de Quarta, 20 Nov 2024") +
+  labs(color = "Tempo de viagem [min]") +
+  theme_bw()
+
+# 2. num domingo de tarde
+# filtramos os dados para o próximo Domingo entre as 21h30 e as 22h30
+stop_times_metro = filter_stop_times(METRO, "2024-11-24", "21:30:00", "22:30:00")
+duracao_viagem = travel_times(stop_times_metro, "Baixa / Chiado") # Baixa / Chiado é a estação de metro da Baixa
+
+summary(duracao_viagem$travel_time/60) # resumo em minutos
+
+duracao_viagem_BC_Dom = ESTACOES_metro |> 
+  left_join(duracao_viagem, by = c("stop_id" = "to_stop_id")) |>  # juntar os dados 
+  filter(!is.na(travel_time)) # remover as plataformas não usadas
+
+# mapa com as estações que se conseguem alcançar em 15, 30 e 45 minutos
+ggplot(duracao_viagem_BC_Dom) +
+  geom_sf(aes(color = travel_time/60))+
+  ggtitle("Alcance desde a estação Baixa-Chiado",
+          subtitle = "às 21h30 de Domingo, 24 Nov 2024") +
+  labs(color = "Tempo de viagem [min]") +
+  theme_bw()
+
+
+
+# Carris Lisboa -----------------------------------------------------------
+
+# Donwload e ler dados
+carris_url = "https://gateway.carris.pt/gateway/gtfs/api/v2.8/GTFS" # 14.3MB
+download.file(carris_url, destfile = "original/carris_gtfs.zip")
+CARRIS = read_gtfs("original/carris_gtfs.zip")
+## Outro pacote
+CARRIS_outro = gtfsrouter::extract_gtfs("original/carris_gtfs.zip")
+carris_transfers = gtfsrouter::gtfs_transfer_table(CARRIS_outro)
+write.csv(carris_transfers[["transfers"]], "original/transfers.txt", row.names = F, quote = F)
+# incluir este ficheiro novamente no zip original
+
+CARRIScomtransfers = read_gtfs("original/carris_gtfs.zip")
+validate_gtfs(CARRIScomtransfers) # validar
+CARRIS = CARRIScomtransfers
+
+# Paragens
+View(CARRIS$stops)
+ESTACOES_carris = stops_as_sf(CARRIS$stops) # converter texto para geometria
+mapview(ESTACOES_carris) # visualizar
+
+
+# Percursos
+ROTAS_carris = tidytransit::shapes_as_sf(CARRIS$shapes) # converter texto para geometria
+plot(ROTAS_carris) # ver as 1739 rotas
+mapview(ROTAS_carris, zcol = "shape_id") # visualizar
+
+
+# 1. num dia útil em hora de ponta
+
+# filtramos os dados para o dia de hoje entre as 7h30 e as 9h00
+stop_times_carris1 = filter_stop_times(CARRIS, "2024-11-20", "07:30:00", "09:00:00")
+duracao_viagem1 = travel_times(stop_times_carris1, "Cais Sodré", stop_dist_check = FALSE)
+
+# estatísticas
+summary(duracao_viagem1$travel_time/60) # resumo em minutos
+hist(duracao_viagem1$travel_time/60, breaks = 20) # histograma
+abline(v = mean(duracao_viagem1$travel_time/60), col = "red") # linha de média
+abline(v = median(duracao_viagem1$travel_time/60), col = "blue") # linha de mediana
+round(prop.table(table(duracao_viagem1$transfers))*100, 1) # percentagem de transferências
+
+duracao_viagem_CS_HP = ESTACOES_carris |> 
+  left_join(duracao_viagem1, by = c("stop_id" = "to_stop_id")) |>  # juntar os dados 
+  filter(transfers >= 1) |> # remover viagens com mais de 1 transferência
+  filter(!is.na(travel_time)) |>  # remover as plataformas não usadas
+  filter(travel_time < 60*60) |> # remover viagens com mais de 60 minutos
+  filter(!stop_id %in% c(19001,14601,14602)) # remover as pagarens em Almada
+  
+# mapa com as estações que se conseguem alcançar em 15, 30 e 45 minutos
+ggplot(duracao_viagem_CS_HP) +
+  geom_sf(aes(color = travel_time/60))+
+  ggtitle("Alcance desde o Cais do Sodré (Carris)",
+          subtitle = "às 7h30 de Quarta, 20 Nov 2024 - máx 1 transf") +
+  labs(color = "Tempo de viagem [min]") +
+  theme_bw()
+
+# 2. num domingo de tarde
+# filtramos os dados para o próximo Domingo entre as 21h30 e as 22h30
+stop_times_carris2 = filter_stop_times(CARRIS, "2024-11-24", "22:00:00", "23:30:00")
+duracao_viagem2 = travel_times(stop_times_carris2, "Cais Sodré", stop_dist_check = FALSE)
+
+summary(duracao_viagem2$travel_time/60) # resumo em minutos
+
+duracao_viagem_CS_Dom = ESTACOES_carris |> 
+  left_join(duracao_viagem2, by = c("stop_id" = "to_stop_id")) |>  # juntar os dados 
+  filter(transfers >= 1) |> # remover viagens com mais de 1 transferência
+  filter(!is.na(travel_time)) |>  # remover as plataformas não usadas
+  filter(travel_time < 60*60) # remover viagens com mais de 60 minutos
+
+# mapa com as estações que se conseguem alcançar em 15, 30 e 45 minutos
+ggplot(duracao_viagem_CS_Dom) +
+  geom_sf(aes(color = travel_time/60))+
+  ggtitle("Alcance desde o Cais do Sodré (Carris)",
+          subtitle = "às 22h00 de Domingo, 24 Nov 2024 - máx 1 transf") +
+  labs(color = "Tempo de viagem [min]") +
+  theme_bw()
+
+
+
+
+
+
+
+
+
+# Carris Metropolitana ------------------------------------------------------------------
+
+# Ler dados
+CARRISm = read_gtfs("original/carris_metropolitana.zip")
+validate_gtfs(CARRISm) # validar
+
+## Outro pacote
+CARRISm_outro = gtfsrouter::extract_gtfs("original/carris_metropolitana.zip")
+carris_transfers = gtfsrouter::gtfs_transfer_table(CARRISm_outro)
+write.csv(carris_transfers[["transfers"]], "original/transfers.txt", row.names = F, quote = F)
+# incluir este ficheiro novamente no zip original
+
+CARRISmcomtransfers = read_gtfs("original/carris_metropolitana.zip")
+validate_gtfs(CARRISmcomtransfers) # validar
+CARRISm = CARRISmcomtransfers
+
+View(CARRISm$stops)
+
+# Paragens
+ESTACOES_carrism = stops_as_sf(CARRISm$stops) # converter texto para geometria
+mapview(ESTACOES_carrism) # visualizar
+mapview(ESTACOES_carris, zcol = "has_shelter") # por tipo de abrigo
+mapview(ESTACOES_carris, zcol = "municipality_name") # por concelho
+
+# Percursos
+ROTAS_carrism = tidytransit::shapes_as_sf(CARRISm$shapes) # converter texto para geometria
+plot(ROTAS_carrism) # ver as 1739 rotas
+mapview(ROTAS_carrism, zcol = "shape_id") # visualizar
+
+
+
+
+
+# upload gtfs com transfers -----------------------------------------------
+
+piggyback::pb_upload("original/carris_metropolitana.zip", "carris_metropolitana.zip", repo = "rosamfelix/PGmob360", tag = "latest")
+piggyback::pb_upload("original/carris_gtfs.zip", "carris_gtfs.zip", repo = "rosamfelix/PGmob360", tag = "latest")
+piggyback::pb_upload("original/metro_gtfs.zip", "metro_gtfs.zip", repo = "rosamfelix/PGmob360", tag = "latest")
